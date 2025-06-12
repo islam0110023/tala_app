3da83f9d1e084d24a22ff852ae7dc760287d2be2
Islam Ramadan <islamalnms123@gmail.com>
2025-06-12 19:34:19 +0300

    feat: Add primary text style
    This commit introduces a `primaryStyle` within the `Styling` abstract class.
    
    - The `primaryStyle` defines a text style with a font size of 30.sp and a font weight of w800.

---
+++ lib/core/utils/styling.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+
+abstract class Styling{
+  static TextStyle primaryStyle = TextStyle(
+    fontSize: 30.sp,
+    fontWeight: FontWeight.w800,
+  );
+}
