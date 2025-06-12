e9d2db28f3b757c67e1f5f99c2e233f048162761
KiiYou <153297852+KiiYou@users.noreply.github.com>
2025-06-13 00:07:42 +0300

    feat: Implement onboarding screen UI
    This commit introduces the UI for the onboarding screen, including text, image, button, and sign-in option. It also adds the Lato font family to the project.
    
    Key changes:
    - Created `DescriptionSizeBoxWidget` for the onboarding description text.
    - Updated `OnboardingScreen` to display the onboarding UI elements:
        - Image asset `assets/image1.png`.
        - `DescriptionSizeBoxWidget`.
        - `CustomButton` for "Start Your Unique Experience!".
        - `CustomSignInOnBoarding` for the "Already have an account? Sign in" text.
    - Added Lato font files to `assets/fonts/`.
    - Configured Lato font family in `pubspec.yaml`.

--- lib/feature/auth/presentation/view/widget/login_screen_body.dart
+++ lib/feature/auth/presentation/view/widget/login_screen_body.dart
@@ -1,10 +1,126 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+import 'package:tala_app/core/utils/asset_image.dart';
+import 'package:tala_app/core/widget/custom_already_sign_in.dart';
+import 'package:tala_app/core/widget/custom_button.dart';
+import 'package:tala_app/core/widget/custom_sign_with_icon.dart';
+import 'package:tala_app/core/widget/custom_text_field.dart';
 
 class LoginScreenBody extends StatelessWidget {
   const LoginScreenBody({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Column();
+    return  Center(
+      child: Column(
+        children: [
+          SizedBox(height: 106),
+          CustomTitle(title:'Ready To Make Unforgettable\nMemories Again?',),
+          SizedBox(height: 49),
+          CustomTextField(hint: 'Email or Phone number'),
+          SizedBox(height: 23),
+          CustomTextField(hint: 'Password', password_flag: true),
+          SizedBox(height: 33),
+          SizedBox(
+            width: 343,
+            child: Text(
+              'Forget Password?',
+              textAlign: TextAlign.center,
+              style: TextStyle(
+                color: const Color(0xFFB147C7),
+                fontSize: 12,
+                fontFamily: 'Lato',
+                fontWeight: FontWeight.w400,
+                decoration: TextDecoration.underline,
+                decorationColor: const Color(0xFFB147C7),
+              ),
+            ),
+          ),
+          SizedBox(height: 36),
+          CustomButton(),
+          SizedBox(height: 167),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Container(
+                width: 40,
+                height: 1,
+                decoration: const BoxDecoration(
+                  gradient: LinearGradient(
+                      colors: [
+                        Color(0xffFFFFFF),
+                        Color(0xff808080),
+                  ])
+                ),
+              ),
+              Padding(
+                padding: EdgeInsets.symmetric(horizontal: 8),
+                child: Text(
+                  'or sign in with',
+                  style: TextStyle(
+                    color: const Color(0xFF808080),
+                    fontSize: 15,
+                    fontFamily: 'Lato',
+                    fontWeight: FontWeight.w600,
+                  ),
+                ),
+              ),
+              Container(
+                width: 40,
+                height: 1,
+                decoration: const BoxDecoration(
+                    gradient: LinearGradient(
+                        colors: [
+                          Color(0xff808080),
+                          Color(0xffFFFFFF),
+
+                        ])
+                ),
+              ),
+            ],
+          ),
+          SizedBox(height: 16,),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              CustomSignWithIcon(img: AppImage.kAppleIcon, onTap: () {  },),
+              SizedBox(width: 20),
+              CustomSignWithIcon(img: AppImage.kGoogleIcon, onTap: () {  },)
+            ],
+          ),
+          SizedBox(height: 32,),
+          CustomAlreadySignIn(onTap: (){}),
+        ],
+      ),
+    );
   }
 }
+
+class CustomTitle extends StatelessWidget {
+  const CustomTitle({
+    super.key, required this.title,
+  });
+  final String title;
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox(
+      width: 309,
+      height: 55,
+      child: Text(
+        title,
+        textAlign: TextAlign.center,
+        style: TextStyle(
+          color: Color(0xFF692A7E),
+          fontSize: 20,
+          fontFamily: 'Lato',
+          fontWeight: FontWeight.w800,
+        ),
+      ),
+    );
+  }
+}
+
+
+
+
