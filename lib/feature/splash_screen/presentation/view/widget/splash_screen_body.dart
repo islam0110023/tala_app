f4bf583501357569d78d227a776b73ab8b852c88
Islam Ramadan <islamalnms123@gmail.com>
2025-06-12 18:33:13 +0300

    update

--- lib/feature/splash_screen/presentation/view/widget/splash_screen_body.dart
+++ lib/feature/splash_screen/presentation/view/widget/splash_screen_body.dart
@@ -17,25 +17,67 @@ class SplashScreenBody extends StatelessWidget {
               begin: Alignment.topRight,
               end: Alignment.bottomLeft,
               stops: [0, .38, .87],
-              transform: GradientRotation(0),
             ),
           ),
         ),
-        Positioned(
-          top: 207.h,
-          left: 70.w,
-          child: SizedBox(
-            width: size.width * .65,
-            child: FittedBox(child: Image.asset("assets/images/logo_tala.png")),
-          ),
-        ),
-        Positioned(
-          top: 410.h,
-          left: 142.w,
-          child: SizedBox(
-            width: size.width * .3,
-            child: FittedBox(child: Image.asset("assets/images/Tala.png")),
-          ),
+        LayoutBuilder(
+          builder: (context, constraints) {
+            if (constraints.maxWidth > 600) {
+              return Center(
+                heightFactor: 2.2.h,
+                child: Stack(
+                  clipBehavior: Clip.none,
+
+                  children: [
+                    Positioned(
+                      child: SizedBox(
+                        width: 400,
+                        child: Image.asset("assets/images/logo_tala.png"),
+                      ),
+                    ),
+                    Positioned(
+                      top: 300,
+                      left: 105,
+                      child: SizedBox(
+                        width: 200,
+                        child: Image.asset(
+                          "assets/images/Tala.png",
+                          fit: BoxFit.contain,
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+              );
+            } else {
+              return Center(
+                heightFactor: 2.5,
+                child: Stack(
+                  clipBehavior: Clip.none,
+
+                  children: [
+                    Positioned(
+                      child: SizedBox(
+                        width: 254,
+                        child: Image.asset("assets/images/logo_tala.png"),
+                      ),
+                    ),
+                    Positioned(
+                      top: 200,
+                      left: 70,
+                      child: SizedBox(
+                        width: 120,
+                        child: Image.asset(
+                          "assets/images/Tala.png",
+                          fit: BoxFit.contain,
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+              );
+            }
+          },
         ),
       ],
     );
