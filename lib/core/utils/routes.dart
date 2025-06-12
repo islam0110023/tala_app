2e7eac475a3a50283b2c6dd9468d2af615a015a1
Islam Ramadan <islamalnms123@gmail.com>
2025-06-12 19:27:20 +0300

    feat: Implement basic onboarding screen structure
    This commit introduces the basic structure for the onboarding screen.
    
    - Creates `OnboardingScreen` as the main widget for the onboarding flow.
    - Creates `OnboardingScreenBody` as a placeholder for the onboarding content.
    - Adds a new route `/onboarding` in `AppRoutes` that currently points to the `SplashScreen` (this will be updated later).

--- lib/core/utils/routes.dart
+++ lib/core/utils/routes.dart
@@ -3,11 +3,16 @@ import 'package:tala_app/feature/splash_screen/presentation/view/splash_screen.d
 
 class AppRoutes {
   static const splashScreen = '/';
+  static const onBoardingScreen = '/onboarding';
 
   static final route = GoRouter(
     routes: [
       GoRoute(
         path: splashScreen,
+        builder: (context, state) => const SplashScreen(),
+      ),
+      GoRoute(
+        path: onBoardingScreen,
         builder: (context, state) => const SplashScreen(),
       ),
     ],
---
+++ lib/feature/onboarding/presentation/view/onboarding_screen.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+import 'package:tala_app/feature/onboarding/presentation/view/widget/onboarding_screen_body.dart';
+
+class OnboardingScreen extends StatelessWidget {
+  const OnboardingScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(
+      body: OnboardingScreenBody(),
+    );
+  }
+}
