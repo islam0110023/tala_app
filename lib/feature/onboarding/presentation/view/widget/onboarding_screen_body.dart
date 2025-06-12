8400dc03e4f67232c5c239fd0a386c256c718f59
Islam Ramadan <islamalnms123@gmail.com>
2025-06-12 21:40:43 +0300

    Refactor: Update OnboardingScreen UI and add logo
    This commit updates the `OnboardingScreen` and `OnboardingScreenBody` widgets to display an onboarding logo and refactors the layout.
    
    - Added `onboarding_logo.png` to the assets.
    - Updated `OnboardingScreenBody` to include the logo, a description widget, a custom button, and a sign-in widget.
    - Refactored `OnboardingScreen` to use the `OnboardingScreenBody` widget.

--- lib/feature/onboarding/presentation/view/widget/onboarding_screen_body.dart
+++ lib/feature/onboarding/presentation/view/widget/onboarding_screen_body.dart
@@ -1,10 +1,33 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:tala_app/core/utils/asset_image.dart';
+import 'package:tala_app/core/widget/custom_already_sign_in.dart';
+import 'package:tala_app/core/widget/custom_button.dart';
+import 'package:tala_app/feature/onboarding/presentation/view/widget/description_sizedbox_widget.dart';
 
 class OnboardingScreenBody extends StatelessWidget {
   const OnboardingScreenBody({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Column();
+    return  Padding(
+      padding: EdgeInsets.symmetric(horizontal: 20.w),
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          SizedBox(height: 70.h),
+          Image.asset(
+            AppImage.kOnboardingLogo,
+            fit: BoxFit.contain,
+          ),
+          SizedBox(height: 70.h),
+          const DescriptionSizeBoxWidget(),
+          SizedBox(height: 40.h),
+          const CustomButton(),
+          SizedBox(height: 31.h),
+          const CustomAlreadySignIn(),
+        ],
+      ),
+    );
   }
 }
