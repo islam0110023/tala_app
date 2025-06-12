53fca7b976a7484bb48afbd283d2da211ff663a2
Islam Ramadan <islamalnms123@gmail.com>
2025-06-12 16:33:55 +0300

    Initial commit

---
+++ android/app/src/main/kotlin/com/example/tala_app/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.tala_app
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
---
+++ lib/main.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const TalaApp());
+}
+
+class TalaApp extends StatelessWidget {
+  const TalaApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+   return MaterialApp();
+  }
+}
+
